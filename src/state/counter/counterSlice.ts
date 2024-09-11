d9b00a6390a855af0b1ddb6a8d127c67b16b7d71
shafeeque <shafeeqsha06@gmail.com>
2024-09-11 20:19:56 +0530

    feat: used async thunk and worked with async apis

--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
     value: number;
@@ -23,9 +23,34 @@ const counterSlice = createSlice({
             state.value += action.payload;
 
         },
+
        
+    },
+    extraReducers: (builder) => {
+        builder.addCase(
+            incrementAsync.pending,
+            () => {
+                console.log("incrementAsync.pending")
+            }
+        ).addCase(
+            incrementAsync.fulfilled,
+            (state, action: PayloadAction<number>) => {
+                state.value = action.payload;
+
+            }
+        )
     }
 
 });
+
+export const incrementAsync = createAsyncThunk(
+    "counter/incrementAsync",
+    async (amount: number) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return amount;
+    }
+)
+
+
 export default counterSlice.reducer;
-export const  {increment, decrement, incrementByAmount} = counterSlice.actions;
+export const  {increment, decrement, incrementByAmount, } = counterSlice.actions;
