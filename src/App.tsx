70876242ab8a18405e8db3310f8f03baeb52c6ff
shafeeque <shafeeqsha06@gmail.com>
2024-09-11 18:12:21 +0530

    feat: add counter slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 
 import { useState } from 'react'
 import './App.css'
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <h2>Redux tutorial</h2>
+      <Counter/>
       
-    </>
+    </div>
   )
 }
 
